09f55254f43a9ded32d702867d2dfc0939b7012b
ALEX <shvitc@gmail.com>
2024-09-21 22:38:53 +0300

    Разделили хедер на компоненты

---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,8 @@
+export default function Logo() {
+	return (
+		<div className='logo'>
+			<img src='logo.png' alt='logo' className='logo__image' />
+			<p className='logo__text'>Запиши</p>
+		</div>
+	);
+}
