dc0a513b3caf8f05572022e28d0a856331a07df6
ALEX <shvitc@gmail.com>
2024-09-23 22:09:07 +0300

    Добавили кнопку (Добавить записку)

--- src/components/Notes/index.jsx
+++ src/components/Notes/index.jsx
@@ -1,10 +1,20 @@
 import Note from "../Note";
 
 import "./notes.scss";
+import "./add-note.scss";
 
 export default function Notes() {
+	function AddNoteCard() {
+		return (
+			<button className='add-note' title='Создать записку'>
+				+
+			</button>
+		);
+	}
+
 	return (
 		<section className='notes'>
+			<AddNoteCard />
 			<Note />
 			<Note />
 			<Note />
